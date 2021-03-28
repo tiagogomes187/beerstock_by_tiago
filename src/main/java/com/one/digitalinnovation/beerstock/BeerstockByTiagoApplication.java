29d9946536ac20060f3b929452b05c51a017f377
Tiago Gomes <tiagogomes187@gmail.com>
2021-03-28 17:21:09 -0300

    Iniciando o projeto BeerStock

---
+++ src/main/java/com/one/digitalinnovation/beerstock/BeerstockByTiagoApplication.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.beerstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerstockByTiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerstockByTiagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/digitalinnovation/beerstock/BeerstockByTiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockByTiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
